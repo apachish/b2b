7655db55783a0a1b6e329a436e2667d6b78b7e94
shahriar pahlevansadegh <apachish@gmail.com>
2019-05-29 11:57:09 +0430

    updTE

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -27,5 +27,6 @@ class DatabaseSeeder extends Seeder
         $this->call(CategorySeeder::class);
         $this->call(PagePositionSeed::class);
         $this->call(UserSeed::class);
+        $this->call(PageSeed::class);
     }
 }
