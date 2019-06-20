d7f6bc653bb4097f46d2c5fe60e7aeeea638c6b1
shahriar pahlevansadegh <apachish@gmail.com>
2019-06-20 19:35:28 +0430

    update

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -18,31 +18,38 @@ class IndexController extends Controller
     public function index()
     {
         $locale = app()->getLocale();
-        $banner_right = Banner::where('banner_position', Banner::BANNER_POSITION_RIGHT)->where('image', '!=', 0)->where('locale',$locale)->where('status', 1)->orderByRaw('RAND()')->skip(0)->take(4)->get();
-        $banner_left = Banner::where('banner_position', Banner::BANNER_POSITION_LEFT)->where('image', '!=', 0)->where('locale',$locale)->where('status', 1)->orderByRaw('RAND()')->skip(0)->take(6)->get();
-        $banner_button = Banner::where('banner_position', Banner::BANNER_POSITION_BOTTOM)->where('image', '!=', 0)->where('locale',$locale)->where('status', 1)->orderByRaw('RAND()')->take(2)->get();
-        $home_scrolling = Banner::where('banner_position', Banner::BANNER_POSITION_HOME_SCROLLING)->where('image', '!=', 0)->where('locale',$locale)->where('status', 1)->orderByRaw('RAND()')->take(4)->get();
-        $leads_buy = Lead::whereAdType('buy')->whereStatus(1)->where('approval_status', 2)->where('locale',$locale)->orderByRaw('RAND()')
+        $banner_right = Banner::where('banner_position', Banner::BANNER_POSITION_RIGHT)->where('image', '!=', 0)->where('locale', $locale)->where('status', 1)->orderByRaw('RAND()')->skip(0)->take(4)->get();
+        $banner_left = Banner::where('banner_position', Banner::BANNER_POSITION_LEFT)->where('image', '!=', 0)->where('locale', $locale)->where('status', 1)->orderByRaw('RAND()')->skip(0)->take(6)->get();
+        $banner_button = Banner::where('banner_position', Banner::BANNER_POSITION_BOTTOM)->where('image', '!=', 0)->where('locale', $locale)->where('status', 1)->orderByRaw('RAND()')->take(2)->get();
+        $home_scrolling = Banner::where('banner_position', Banner::BANNER_POSITION_HOME_SCROLLING)->where('image', '!=', 0)->where('locale', $locale)->where('status', 1)->orderByRaw('RAND()')->take(4)->get();
+        $leads_buy = Lead::whereAdType('buy')->whereStatus(1)->where('approval_status', 2)->where('locale', $locale)->orderByRaw('RAND()')
             ->where('publish_at', '>', Carbon::now()->subDay(3))
             ->take(30)->get();
-        $leads_sell = Lead::whereAdType('sell')->whereStatus(1)->where('approval_status', 2)->where('locale',$locale)->orderByRaw('RAND()')
+        $leads_sell = Lead::whereAdType('sell')->whereStatus(1)->where('approval_status', 2)->where('locale', $locale)->orderByRaw('RAND()')
             ->where('publish_at', '>', Carbon::now()->subDay(3))
             ->take(30)->get();
         $products_featured = Lead::whereStatus(1)->with(['user', 'medias' => function ($q) {
             $q->where('is_default', true);
-        }, 'categories'])->where('locale',$locale)->where('approval_status', 2)->orderByRaw('RAND()')->take(30)->get();
+        }, 'categories'])->where('locale', $locale)->where('approval_status', 2)->orderByRaw('RAND()')->take(30)->get();
 
-        $categories = Category::get()->toTree();
+        $categories = cache()->rememberForever('category_tree', function () {
+            return Category::get()->toTree();
+        });
         $period_array = [
             ["start" => 0, 'finish' => 18],
             ["start" => 18, 'finish' => 36],
             ["start" => 33, 'finish' => 51],
         ];
         $period = $period_array[0];
-        $categories_images = Category::whereFeature(true)->where('image', '!=', 'noImage.png')->get()->toTree();
+        $categories_images = cache()->rememberForever('category_featured_image', function () {
+           return Category::whereFeature(true)->where('image', '!=', 'noImage.png')->get()->toTree();
+        });
         $company_featured = User::with('category')->where('featured_company', 1)->whereStatus(1)->orderByRaw('RAND()')->take(6)->get();
-        $testimonials = Testimonial::whereStatus(1)->orderBy('created_at', 'desc')->where('locale',$locale)->take(10)->get();
-        $articles = Article::with('user')->whereStatus(1)->orderBy('sort_order', 'Asc')->take(2)->get();
+        $testimonials = Testimonial::whereStatus(1)->orderBy('created_at', 'desc')->where('locale', $locale)->take(10)->get();
+        $articles = cache()->remember('articles_index', 86400, function () {
+            return Article::with('user')->whereStatus(1)->orderBy('sort_order', 'Asc')->take(2)->get();
+
+        });
         return view('index', compact('banner_right', 'home_scrolling', 'banner_left', 'banner_button'
             , 'period', 'categories_images',
             'leads_buy',
@@ -82,8 +89,8 @@ class IndexController extends Controller
         if (!in_array($lang, ['en', 'fa'])) {
             $lang = 'fa';
         }
-        cache()->forget(session_id()."_locale");
-        session()->put('locale',$lang);
+        cache()->forget(session_id() . "_locale");
+        session()->put('locale', $lang);
         return redirect(url(url()->previous()));
 
     }
