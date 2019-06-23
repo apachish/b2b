c54ad45dde9a02fffbbe91ebb2c4d0fb6a8f8ca6
shahriar pahlevansadegh <apachish@gmail.com>
2019-06-23 09:47:14 +0430

    a

--- resources/views/articles.blade.php
+++ resources/views/articles.blade.php
@@ -1,19 +1,21 @@
 @if (!empty($articles))
     @for($i=0;$i<1;$i++)
-<div class="articles col-lg-4 col-md-4 col-xs-12">
-    <p class="morelink"><a href="{{route('home.articles')}}"
-                           title="View All">{{__("messages.View All")}}  </a></p>
-    <h3 class="title-cat">{{__("messages.Articles")}}  </h3>
-    <div class="bodyarticles">
-        <p class="art-title"><a
-                href="{{route('home.articles',['article-slug'=>$articles[0]->slug])}}">{{$articles[$i]->title}}</a>
-        </p>
-        <p class="art-date">{{ __("messages.Posted on")}}
-            : {{$articles[$i]->updated_at}}
-            - {{__("messages.by")}} {{$articles[$i]->user->getDisplayName()}}</p>
+        @if(!empty($articles[$i]))
+            <div class="articles col-lg-4 col-md-4 col-xs-12">
+                <p class="morelink"><a href="{{route('home.articles')}}"
+                                       title="View All">{{__("messages.View All")}}  </a></p>
+                <h3 class="title-cat">{{__("messages.Articles")}}  </h3>
+                <div class="bodyarticles">
+                    <p class="art-title"><a
+                                href="{{route('home.articles',['article-slug'=>$articles[$i]->slug])}}">{{$articles[$i]->title}}</a>
+                    </p>
+                    <p class="art-date">{{ __("messages.Posted on")}}
+                        : {{$articles[$i]->updated_at}}
+                        - {{__("messages.by")}} {{$articles[$i]->user->getDisplayName()}}</p>
 
-        <p class="art-des">{{short_string($articles[$i]->description,200)}},....</p>
-    </div>
-</div>
-@endfor
+                    <p class="art-des">{{short_string($articles[$i]->description,200)}},....</p>
+                </div>
+            </div>
+        @endif
+    @endfor
 @endif
